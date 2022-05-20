81365567c3b296d6970392bc87b6ec212b21d0f8
小林　誠 <kobayashi.makoto@aiaruku.com>
2022-05-20 16:02:35 +0900

    小林チェック

---
+++ Lesson_04-05/sample/lesson_04-05_sample/lesson_04-05_sample.ino
@@ -0,0 +1,117 @@
+#define speedPinR 9    //  右側のPWM信号を送信するピンの設定
+#define RightMotorDirPin1  12    //右後ろのモーターの信号ピンの設定 
+#define RightMotorDirPin2  11    //右前のモーターの信号ピンの設定
+#define speedPinL 6    // 左のPWM信号を送信するピンの設定
+#define LeftMotorDirPin1  7    //左後ろのモーターの信号ピンの設定 
+#define LeftMotorDirPin2  8   //左前のモーターの信号ピンの設定  
+
+
+//モーターの制御
+void go_Advance(int speed = 200, int time = 0)  //前に進む関数
+{
+  digitalWrite(RightMotorDirPin1, HIGH);
+  digitalWrite(RightMotorDirPin2, LOW);
+  digitalWrite(LeftMotorDirPin1, HIGH);
+  digitalWrite(LeftMotorDirPin2, LOW);
+  analogWrite(speedPinL, speed);
+  analogWrite(speedPinR, speed);
+  if (time == 0) {
+    ;
+  } else {
+    delay(time);
+  }
+}
+void go_Left(int speed = 200, int time = 0) //左に旋回する関数
+{
+  digitalWrite(RightMotorDirPin1, HIGH);
+  digitalWrite(RightMotorDirPin2, LOW);
+  digitalWrite(LeftMotorDirPin1, LOW);
+  digitalWrite(LeftMotorDirPin2, HIGH);
+  analogWrite(speedPinL, speed);
+  analogWrite(speedPinR, speed);
+  if (time == 0) {
+    ;
+  } else {
+    delay(time);
+  }
+}
+void go_Right(int speed = 200, int time = 0) //右に旋回する関数
+{
+  digitalWrite(RightMotorDirPin1, LOW);
+  digitalWrite(RightMotorDirPin2, HIGH);
+  digitalWrite(LeftMotorDirPin1, HIGH);
+  digitalWrite(LeftMotorDirPin2, LOW);
+  analogWrite(speedPinL, speed);
+  analogWrite(speedPinR, speed);
+  if (time == 0) {
+    ;
+  } else {
+    delay(time);
+  }
+}
+void go_Back(int speed = 200, int time = 0) //後ろに下がる関数
+{
+  digitalWrite(RightMotorDirPin1, LOW);
+  digitalWrite(RightMotorDirPin2, HIGH);
+  digitalWrite(LeftMotorDirPin1, LOW);
+  digitalWrite(LeftMotorDirPin2, HIGH);
+  analogWrite(speedPinL, speed);
+  analogWrite(speedPinR, speed);
+  if (time == 0) {
+    ;
+  } else {
+    delay(time);
+  }
+}
+void stop_Stop(int time = 1000)    //ストップ
+{
+  digitalWrite(RightMotorDirPin1, LOW);
+  digitalWrite(RightMotorDirPin2, LOW);
+  digitalWrite(LeftMotorDirPin1, LOW);
+  digitalWrite(LeftMotorDirPin2, LOW);
+  delay(time);
+}
+//モーター速度の設定
+void set_Motorspeed(int speed_L, int speed_R)
+{
+  analogWrite(speedPinL, speed_L);
+  analogWrite(speedPinR, speed_R);
+}
+
+//ピンの初期化
+void init_GPIO()
+{
+  pinMode(RightMotorDirPin1, OUTPUT);
+  pinMode(RightMotorDirPin2, OUTPUT);
+  pinMode(speedPinL, OUTPUT);
+
+  pinMode(LeftMotorDirPin1, OUTPUT);
+  pinMode(LeftMotorDirPin2, OUTPUT);
+  pinMode(speedPinR, OUTPUT);
+  stop_Stop();
+}
+
+void setup()
+{
+  init_GPIO();
+  go_Advance();//前に進む関数
+ 
+  delay(2000);
+  
+  go_Back();//後ろに下がる関数
+ 
+  delay(2000);
+  
+  go_Left();//左に旋回する関数
+ 
+  delay(2000);
+  
+  go_Right();//右に旋回する関数
+ 
+  delay(2000);
+  
+  stop_Stop();//ストップ
+}
+
+void loop() {
+} 
---
+++ Lesson_08/sample/lesson_08_arduino_challenge_sample/lesson_08_arduino_sample/lesson_08_arduino_sample.ino
@@ -0,0 +1,21 @@
+#include <IRremote.h>  // IRRemote.hをインクルード
+const int irReceiverPin = 2;  ///受信モジュールのSIGはpin2
+IRrecv irrecv(irReceiverPin); //IRrecvタイプの変数を作成します
+decode_results results;    // 結果
+
+void setup(){
+  Serial.begin(9600);    //シリアルを初期化し、ボーレートは9600に設定する
+  irrecv.enableIRIn();   // 赤外線受信機モジュールを有効にする
+  Serial.print("赤外線モジュールサンプルプログラムスタート\n");
+}
+
+void loop(){
+  if (irrecv.decode(&results)){ //赤外線受信機モジュールの受信データ
+    Serial.print("IRコード: ");
+    Serial.print(results.value); //16進数を出力する
+    Serial.print(",　ビット: ");
+    Serial.println(results.bits); //bitsを結果に出力する
+    irrecv.resume();// 次の値を受取る
+  }
+  delay(600); //600ミリ秒待機
+}
